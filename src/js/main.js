f554813310c007b1ea78e2af04f151994f10c5c1
Jongmin Kim <posthink2@gmail.com>
2017-02-21 14:31:43 +1100

    Added React and devdependencies to the app

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import {question1} from './questions/question1.js';
 import {question2} from './questions/question2.js';
 import {question3} from './questions/question3.js';
@@ -6,23 +9,24 @@ import {question5} from './questions/question5.js';
 import {question6} from './questions/question6.js';
 import {question7} from './questions/question7.js';
 
+var questions = [
+    {title: "The Multiples of 3 and 5 is", answer: question1(1000)},
+    {title: "The sum of even Fibonacci numbers", answer: question2(4000000)},
+    {title: "Largest prime factor is", answer: question3(600851475143)},
+    {title: "Largest palindrome product is", answer: question4()},
+    {title: "Smallest multiple is ", answer: question5(1, 20)},
+    {title: "Sum square difference is ", answer: question6()},
+    {title: "10000th prime number is ", answer: question7(1e4 + 1)}
+];
 
+var questionLIs = questions.map(function(item, i){
+    return <li key={'question_' + i}>{item.title}{item.answer}</li>
+});
 
-function display(number, msg, fn) {
-    var element = document.createElement('p');
-    element.innerText = number + ". " + msg + " " + fn;
-    document.body.appendChild(element);
-}
+var Answers = React.createClass({
+    render: function () {
+        return <ol>{questionLIs}</ol>
+    }
+});
 
-(function () {
-    var time = new Date().getTime();
-    display(1, "The Multiples of 3 and 5 is", question1(1000));
-    display(2, "The sum of even Fibonacci numbers", question2(4000000));
-    display(3, "Largest prime factor is", question3(600851475143));
-    display(4, "Largest palindrome product is", question4());
-    display(5, "Smallest multiple is ", question5(1, 20));
-    display(6, "Sum square difference is ", question6());
-    display(7, "10000th prime number is ", question7(1e4 + 1));
-    return console.log((Date.now() - time)/1000, 'seconds');
-})(window);
-
+ReactDOM.render(<Answers />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 /**
  * Created by jongmin.kim on 10/02/2017.
  */
-
 var path = require('path');
 
 module.exports = {
     entry: './src/js/main.js',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
+            }
+        ]
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './src/js/dist')
